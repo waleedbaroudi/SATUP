54a2a4a15169d6195a071835d51d8c8cfe79868a
anilkul98 <42135771+anilkul98@users.noreply.github.com>
2019-12-13 20:40:33 +0300

    first commit

---
+++ SATUP/SATUP/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SATUP
+//
+//  Created by user on 13.12.2019.
+//  Copyright © 2019 wab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "new title"
+    }
+
+
+}
+
