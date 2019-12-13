aa1c4fedb4f8380815aae1d4336f355d9652a964
Abdul <manofsteel272@yahoo.com>
2019-12-13 20:44:20 +0300

    READ sadwfiqwf

--- SATUP/SATUP/ViewController.swift
+++ SATUP/SATUP/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        title = "new title"
+        title = "Moonlight sonata is life"
     }
 
 
