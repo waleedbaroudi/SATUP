977f6c646be33360be4498b6917ad4df66fc0247
anilkul98 <42135771+anilkul98@users.noreply.github.com>
2019-12-13 21:18:19 +0300

    added info

---
+++ SATUP/SATUP/ViewControllers/info.swift
@@ -0,0 +1,33 @@
+//
+//  info.swift
+//  SATUP
+//
+//  Created by user on 13.12.2019.
+//  Copyright © 2019 wab. All rights reserved.
+//
+
+import UIKit
+
+class info: UIViewController {
+
+    @IBAction func dismissButton(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
