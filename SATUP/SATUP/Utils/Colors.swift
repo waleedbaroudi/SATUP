010898ee86897bd467ef7d83847f6bd76175ce06
anilkul98 <42135771+anilkul98@users.noreply.github.com>
2019-12-14 16:16:16 +0300

    added BG, Colors class, and changed bar color

---
+++ SATUP/SATUP/Utils/Colors.swift
@@ -0,0 +1,12 @@
+//
+//  Colors.swift
+//  SATUP
+//
+//  Created by user on 14.12.2019.
+//  Copyright © 2019 wab. All rights reserved.
+//
+
+import Foundation
+struct Colors{
+    static let BLACK: String = "#ffffff"
+}
