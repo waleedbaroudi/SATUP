cf632c5b43006031154cb6b551466406bbedaf3b
anilkul98 <42135771+anilkul98@users.noreply.github.com>
2020-01-21 19:10:04 +0300

    fixed  loading indicator

--- SATUP/SATUP/Model/VocabularyDataSource.swift
+++ SATUP/SATUP/Model/VocabularyDataSource.swift
@@ -24,10 +24,11 @@ class VocabularyDataSource{
             request.addValue("application/json", forHTTPHeaderField: "Content-Type")
             let dataTask = Network.session.dataTask(with: request) { (data, response, error) in
                 var vocabList: [Vocab] = []
-                do {vocabList = try Network.decoder.decode([Vocab].self, from: data!)} catch{}//TODO: HANDLE THIS
-                DispatchQueue.main.async {
-                    self.delegate?.vocabLoaded(vocabList: vocabList)
-                }
+                do {vocabList = try Network.decoder.decode([Vocab].self, from: data!)
+                    DispatchQueue.main.async {
+                        self.delegate?.vocabLoaded(vocabList: vocabList)
+                    }
+                } catch{}//TODO: HANDLE THIS
             }
             dataTask.resume()
         }
